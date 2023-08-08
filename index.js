dea2dc176184b3af44674b7197a7b11810eef6c3
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2023-08-07 21:15:23 -0700

    Add deck/card schema testing

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ const print_flashcards = {
                         front: {
                             type: 'string',
                             description:
-                                'Front content of a flashcard containing a question',
+                                'Front content of a flashcard containing a question. Quotation marks should be escaped with a forward slash.',
                         },
                         back: {
                             type: 'string',
                             description:
-                                'Back content of a flashcard containing the answer to the question',
+                                'Back content of a flashcard containing the answer to the question. Quotation marks should be escaped with a forward slash.',
                         },
                     },
                 },
@@ -37,7 +37,7 @@ const print_flashcards = {
 };
 
 const promptBuilder = (cardCount, topics, keywords, example, difficulty) => {
-    let prompt = `The user will provide you with a set of text which they would like to print ${cardCount} flashcards.`;
+    let prompt = `The user will provide you with a set of text which they would like to print ${cardCount} flashcard(s). `;
     if (topics) {
         prompt += `They have indicated the topic(s) they would like the flashcards to be about are the following: ${topics.join(
             ', '
@@ -49,7 +49,7 @@ const promptBuilder = (cardCount, topics, keywords, example, difficulty) => {
         )}. `;
     }
     if (example) {
-        prompt += `They have provided an example question and answer to help guide the generation of cards. Here is the example front content of the card: ${example.front} and here is the example back content of the card: ${example.back}. `;
+        prompt += `They have provided an example question and answer to help guide the generation of cards. Here is the example front content of the card: "${example.front}" and here is the example back content of the card: "${example.back}". `;
     }
     if (difficulty) {
         `The user has indicated that they would like the difficulty level of the cards to be of ${difficulty} difficulty. `;
@@ -74,23 +74,71 @@ const generate = async (inputText, prompt) => {
         functions: [print_flashcards],
     });
     const { arguments } = chatCompletion.data.choices[0].message.function_call;
-    const { flashcards } = JSON.parse(arguments);
-    console.log(flashcards);
+    const { usage } = chatCompletion.data;
+    console.log(chatCompletion);
+    return arguments;
 };
 
 const input =
     'In computing, plain text is a loose term for data (e.g. file contents) that represent only characters of readable material but not its graphical representation nor other objects (floating-point numbers, images, etc.). It may also include a limited number of "whitespace" characters that affect simple arrangement of text, such as spaces, line breaks, or tabulation characters. Plain text is different from formatted text, where style information is included; from structured text, where structural parts of the document such as paragraphs, sections, and the like are identified; and from binary files in which some portions must be interpreted as binary objects (encoded integers, real numbers, images, etc.) The term is sometimes used quite loosely, to mean files that contain only "readable" content (or just files with nothing that the speaker does not prefer). For example, that could exclude any indication of fonts or layout (such as markup, markdown, or even tabs); characters such as curly quotes, non-breaking spaces, soft hyphens, em dashes, and/or ligatures; or other things. In principle, plain text can be in any encoding, but occasionally the term is taken to imply ASCII. As Unicode-based encodings such as UTF-8 and UTF-16 become more common, that usage may be shrinking. Plain text is also sometimes used only to exclude "binary" files: those in which at least some parts of the file cannot be correctly interpreted via the character encoding in effect. For example, a file or string consisting of "hello" (in any encoding), following by 4 bytes that express a binary integer that is not a character, is a binary file. Converting a plain text file to a different character encoding does not change the meaning of the text, as long as the correct character encoding is used. However, converting a binary file to a different format may alter the interpretation of the non-textual data. According to The Unicode Standard: "Plain text is a pure sequence of character codes; plain Un-encoded text is therefore a sequence of Unicode character codes.In contrast, styled text, also known as rich text, is any text representation containing plain text plus added information such as a language identifier, font size, color, hypertext links, and so on. SGML, RTF, HTML, XML, and TeX are examples of rich text fully represented as plain text streams, interspersing plain text data with sequences of characters that represent the additional data structures."';
 
-const prompt = promptBuilder(
-    5,
-    ['computer science', 'encoding'],
-    ['text'],
-    {
-        front: 'What is 1+1?',
-        back: '2',
-    },
-    'very easy'
-);
-console.log(prompt);
+const prompt = promptBuilder(3);
 
-generate(input, prompt);
+const isValidCard = (card) => {
+    return (
+        typeof card === 'object' &&
+        card.front &&
+        typeof card.front === 'string' &&
+        card.back &&
+        typeof card.back === 'string'
+    );
+};
+
+const isValidResponse = (response) => {
+    if (typeof response !== 'object' || !response.flashcards) {
+        console.error('Invalid response');
+        return false;
+    }
+    const { flashcards } = response;
+
+    if (!Array.isArray(flashcards)) {
+        console.error('Flashcards is not an array');
+        return false;
+    }
+
+    for (let card of flashcards) {
+        if (!isValidCard(card)) {
+            console.error('Invalid card:', card);
+            return false;
+        }
+    }
+    return true;
+};
+
+const runTest = async (input, prompt) => {
+    for (let i = 0; i < 1; i++) {
+        const response = await generate(input, prompt);
+        console.log(i);
+        console.log(response);
+        let json;
+        try {
+            json = JSON.parse(response);
+        } catch (err) {
+            console.error(err);
+        }
+
+        const result = isValidResponse(json);
+        console.log(result);
+    }
+};
+runTest(input, prompt);
+
+// TODO get count of tokens used by request
+// TODO should report failure rate
+// TODO too many cards are being returned if requested cards < 3
+
+// Returning a flashcard with quotation marks may cause issues parsing json
+// "front": "What does it mean when plain text is referred to as "loose"?",
+// check response status
+// handle rate limiting
+// implement max tokens
